4c328158e59e8cdb2d3ad58e22d9f7c026bc33d4
Emre Kovanci <kovanciemre@gmail.com>
2025-02-27 15:47:17 +0300

    add comments to compiler warning flags for clarity

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -24,28 +24,28 @@ function(set_compiler_warnings target)
                     /utf-8
                     /diagnostics:caret
                     /sdl
-                    /w14165
-                    /w44242
-                    /w44254
-                    /w44263
-                    /w34265
-                    /w34287
-                    /w44296
-                    /w44388
-                    /w44464
-                    /w14545
-                    /w14546
-                    /w14547
-                    /w14549
-                    /w14555
-                    /w34619
-                    /w34640
-                    /w24826
-                    /w14905
-                    /w14906
-                    /w14928
-                    /w45038
                     /W4
+                    /w14165 # 'HRESULT' is being converted to 'bool'; are you sure this is what you want?
+                    /w44242 # 'identifier': conversion from 'type1' to 'type2', possible loss of data
+                    /w44254 # 'operator': conversion from 'type1':'field_bits' to 'type2':'field_bits', possible loss of data
+                    /w44263 # 'function' : member function does not override any base class virtual member function
+                    /w34265 # 'classname': class has virtual functions, but its non-trivial destructor is not virtual; instances of this class may not be destructed correctly
+                    /w34287 # 'operator' : unsigned/negative constant mismatch
+                    /w44296 # 'operator' : expression is always false
+                    /w44388 # 'token' : signed/unsigned mismatch
+                    /w44464 # relative include path contains '..'
+                    /w14545 # expression before comma evaluates to a function which is missing an argument list
+                    /w14546 # function call before comma missing argument list
+                    /w14547 # 'operator' : operator before comma has no effect; expected operator with side-effect
+                    /w14549 # 'operator' : operator before comma has no effect; did you intend 'operator'?
+                    /w14555 # expression has no effect; expected expression with side-effect
+                    /w34619 # #pragma warning : there is no warning number 'number'
+                    /w34640 # 'instance' : construction of local static object is not thread-safe
+                    /w24826 # Conversion from 'type' to 'type' is sign-extended. This may cause unexpected runtime behavior.
+                    /w14905 # wide string literal cast to 'LPSTR'
+                    /w14906 # string literal cast to 'LPWSTR'
+                    /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
+                    /w45038 # data member 'member1' will be initialized after data member 'member2', data member 'member' will be initialized after base class 'base_class'
                     /permissive-
                     /volatile:iso
                     /Zc:inline
